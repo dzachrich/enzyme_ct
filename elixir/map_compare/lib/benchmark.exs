7667c14d19de42652996f11b7f8d46f9999493c9
David Zachrich <dave@zachrich.com>
2023-02-20 17:37:17 -0500

    additional README benchmark updates (elixir)

--- elixir/map_compare/lib/benchmark.exs
+++ elixir/map_compare/lib/benchmark.exs
@@ -1,5 +1,5 @@
-list = Enum.to_list(1..10_000)
-map_fun = fn i -> [i, i * i] end
+#list = Enum.to_list(1..10_000)
+#map_fun = fn i -> [i, i * i] end
 
 m1 = %{
   "a" => "this",
@@ -23,8 +23,6 @@ s1 = %{"str" => "abc", "num" => 10, "str2" => "this"}
 s2 = %{"str" => "abc", "num" => 10}
 
 Benchee.run(%{
-  "flat_map" => fn -> Enum.flat_map(list, map_fun) end,
-  "map.flatten" => fn -> list |> Enum.map(map_fun) |> List.flatten() end,
   #    "nested" => fn -> MapCompare.compare(m1, m2) end,
   "subset small" => fn -> MapCompare.compare(s1, s2) end,
   "superset small" => fn -> MapCompare.compare(s2, s1) end
